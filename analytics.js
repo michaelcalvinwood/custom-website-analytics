678d81dfad13d659a0f6b601ffc5a206bf034de0
Michael Wood <michaelwood33311@icloud.com>
2022-12-12 12:00:06 -0500

    added https server integrated with web sockets

--- analytics.js
+++ analytics.js
@@ -1,3 +1,36 @@
 const ws = require('ws');
+const listenPort = 5100;
+const privateKeyPath = '/etc/letsencrypt/live/node.pymnts.com/privkey.pem';
+const fullchainPath = '/etc/letsencrypt/live/node.pymnts.com/fullchain.pem';
 
-const wss = new ws.WebSocketServer({ port: 8080 });
+const express = require('express');
+const https = require('https');
+const cors = require('cors');
+const fs = require('fs');
+
+const app = express();
+app.use(express.static('public'));
+app.use(express.json({limit: '200mb'})); 
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+const server = https.createServer({
+    key: fs.readFileSync(privateKeyPath),
+    cert: fs.readFileSync(fullchainPath),
+  }, app);
+  
+
+const wss = new ws.WebSocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+      console.log('received: %s', data);
+    });
+  
+    ws.send('something');
+  });
+  
+  server.listen(listenPort);
