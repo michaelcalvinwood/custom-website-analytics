6f2e38a38362bddde25ff3e0e0ea37418325e997
Michael Wood <michaelwood33311@icloud.com>
2022-12-13 09:45:16 -0500

    fixed typo in YYYY-MM-DD

--- analytics.js
+++ analytics.js
@@ -1,7 +1,7 @@
 const ws = require('ws');
 const listenPort = 5100;
-const privateKeyPath = '/etc/letsencrypt/live/node.pymnts.com/privkey.pem';
-const fullchainPath = '/etc/letsencrypt/live/node.pymnts.com/fullchain.pem';
+const privateKeyPath = '/etc/letsencrypt/live/analytics.pymnts.com/privkey.pem';
+const fullchainPath = '/etc/letsencrypt/live/analytics.pymnts.com/fullchain.pem';
 
 const express = require('express');
 const https = require('https');
@@ -12,6 +12,13 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json({limit: '200mb'})); 
 app.use(cors());
+
+const todaysLocalDateAsYyyyMmDd = () => {
+  let yourDate = new Date()
+  const offset = yourDate.getTimezoneOffset()
+  yourDate = new Date(yourDate.getTime() - (offset*60*1000))
+  return yourDate.toISOString().split('T')[0]
+}
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
@@ -25,12 +32,14 @@ const server = https.createServer({
 
 const wss = new ws.WebSocketServer({ server });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, req) {
+    console.log('connection');
     ws.on('message', function message(data) {
-      console.log('received: %s', data);
+      console.log('%s: %s', todaysLocalDateAsYyyyMmDd(), data);
+      
     });
   
     ws.send('something');
   });
   
-  server.listen(listenPort);
+server.listen(listenPort);
