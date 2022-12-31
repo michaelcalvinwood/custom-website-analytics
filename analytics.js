293a3e88d421f84551cef3c1ec2555cf15cf1729
Michael Wood <michaelwood33311@icloud.com>
2022-12-31 10:44:26 -0500

    added subscribing to url and sending pageInfo to subsribers in realtime as pageInfo changes

--- analytics.js
+++ analytics.js
@@ -51,48 +51,107 @@ const server = https.createServer({
   
 const wss = new ws.WebSocketServer({ server });
 
+const urlSet = {};
+const subscriptions = {};
+const connections = {};
+
+const subscribe = (uuid, url, ws) => {
+  console.log(`subscribe: ${uuid} to ${url}`);
+
+  if (typeof connections[uuid] === 'undefined') connections[uuid] = ws;
+
+  if (typeof subscriptions[url] === 'undefined') subscriptions[url] = new Set();
+  subscriptions[url].add(uuid);
+ 
+  if (typeof subscriptions[uuid] === 'undefined') subscriptions[uuid] = new Set();
+  subscriptions[uuid].add(url);
+}
+
+const handleSubscriptions = async url => {
+  if (typeof subscriptions[url] === 'undefined') return;
+  let info = {};
+  info.type = 'urlInfo';
+  info.url = url;
+
+  currentDate = todaysLocalDateAsYyyyMmDd();
+  let key = `${currentDate}|${url}|uniquePageViewers`;
+  info.uniquePageViewers = await redisClient.get(key);
+
+  key = `${currentDate}|${url}|timeOnPage`;
+  info.timeOnPage = await redisClient.get(key);
+
+  subscriptions[url].forEach(uuid => {
+    connections[uuid].send(JSON.stringify(info));
+  })
+}
+
+const unsubscribeAll = (uuid) => {
+  if (typeof connections[uuid] !== 'undefined') delete connections[uuid];
+
+  if (typeof subscriptions[uuid] !== 'undefined') {
+    subscriptions[uuid].forEach(url => subscriptions[url].delete(uuid));
+    delete subscriptions[uuid];
+  }
+
+  console.log(`unsubscribe: ${uuid} from all`);
+}
+
 const processMessage = async (info, ws) => {
-  //return new Promise (async (resolve, reject) => {
-    console.log(JSON.stringify(wss.clients, null, 4));
+  let data = JSON.parse(info);
+    
+  if (typeof data.type === undefined) return;
 
-    let data = JSON.parse(info);
-      
-    if (typeof data.type === undefined) return;
+  if (!redisConnected) return;
 
-    if (!redisConnected) return;
+  let currentDate, key, incVal,result, totalPageViews, uniquePageViews, timeOnPage, url;
 
-    let currentDate, key, incVal;
+  switch (data.type) {
+    case 'pageVisit':
+      url = data.host + data.path;
+        ws.uuid = data.uuid;
+        ws.url = url;
+        
+        currentDate = todaysLocalDateAsYyyyMmDd();
+        key = `${currentDate}|${url}|timeOnPage`;
+        incVal = data.ts;
+        await redisClient.INCRBY(key, incVal);
+        console.log(key, incVal);
+        
+        key = `${currentDate}|${url}|pageViews`;
+        incVal = 1;
+        totalPageViews = await redisClient.INCRBY(key, 1);
+        console.log(key, incVal);
 
-    switch (data.type) {
-      case 'pageVisit':
-          ws.uuid = data.uuid;
-          currentDate = todaysLocalDateAsYyyyMmDd();
-          key = `${currentDate}|${data.uuid}|${data.ip}|${data.host}|${data.path}|${data.query}`;
-          incVal = data.ts;
-          await redisClient.INCRBY(key, incVal);
-          console.log(key, incVal);
-          
-          key = `${currentDate}|${data.path}`;
+        key = `${currentDate}|${url}|viewers`;
+        result = await redisClient.SADD(key, data.uuid);
+
+        if (result) {
+          key = `${currentDate}|${url}|uniquePageViewers`;
           incVal = 1;
-          await redisClient.INCRBY(key, 1);
+          uniquePageViews = await redisClient.INCRBY(key, 1);
           console.log(key, incVal);
+        }
 
-          return;
-        break;
-      case 'pageStay':
-          currentDate = todaysLocalDateAsYyyyMmDd();
-          key = `${currentDate}|${data.uuid}|${data.ip}|${data.host}|${data.path}|${data.query}`;
-          incVal = data.ts;
-          await redisClient.INCRBY(key, incVal);
-          console.log(key, incVal);
-          
-          return;
-        break;
-      default:
-          console.error(`unknown type: ${data.type}`)
-          return;
-    }
-  //})
+        subscribe(data.uuid, url, ws);
+
+        handleSubscriptions(url);
+        return;
+      break;
+    case 'pageStay':
+        currentDate = todaysLocalDateAsYyyyMmDd();
+        url = data.host + data.path;
+
+        key = `${currentDate}|${url}|timeOnPage`;
+        incVal = data.ts;
+        timeOnPage = await redisClient.INCRBY(key, incVal);
+        
+        handleSubscriptions(url);
+        return;
+      break;
+    default:
+        console.error(`unknown type: ${data.type}`)
+        return;
+  }
 }
 
 wss.on('connection', (ws, req) => {
@@ -103,6 +162,8 @@ wss.on('connection', (ws, req) => {
     });
 
     ws.on('close', () => {
+      unsubscribeAll(ws.uuid);
+
       console.log('closed connection', ws.uuid, wss.clients.size);
     })
   });
